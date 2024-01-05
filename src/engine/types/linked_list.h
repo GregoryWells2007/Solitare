00da2fcaf759f64d5476a48b2f8cfa701b2b6774
Gregory Wells <gregowells07@gmail.com>
2024-01-04 21:26:35 -0500

    oops i forgot that i needed a c file

--- src/engine/types/linked_list.h
+++ src/engine/types/linked_list.h
@@ -2,49 +2,12 @@
 
 typedef struct linked_list {
     void* data;
-    linked_list* next;
+    struct linked_list* next;
 } linked_list;
 
-linked_list linked_list_create() {
-    linked_list list = {};
-    list->data = NULL;
-    list->next = NULL;
-    return list;
-}
+linked_list linked_list_create();
 
-void linked_list_add(linked_list* list, void* data) {
-    while (list->data != NULL)
-        list = list->next;
-
-    list->data = data;
-    list->next = &linked_list_create();
-}
-
-void* linked_list_get(linked_list* list, int index) {
-    while (index > 0) {
-        list = list->next;
-        index--;
-    }
-    return list->data;
-}
-
-void linked_list_set(linked_list* list, int index, void* data) {
-    while (index > 0) {
-        index--;
-        list = list->next;
-    }
-    list->data = data;
-}
-
-int linked_list_size(linked_list* list) {
-    int count = 0;
-    if (list->data != NULL)
-        count++;
-
-    while (list->next != NULL) {
-        count++;
-        list = list->next;
-    }
-
-    return count;
-}
+void linked_list_add(linked_list* list, void* data);
+void* linked_list_get(linked_list* list, int index);
+void linked_list_set(linked_list* list, int index, void* data);
+int linked_list_size(linked_list* list);
