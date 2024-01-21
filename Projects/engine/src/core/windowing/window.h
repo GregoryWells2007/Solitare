2a5ee5637a532747b3e3908cf917ea2050636021
Gregory Wells <gregowells07@gmail.com>
2024-01-21 10:40:38 -0500

    fix header defs

--- Projects/engine/src/core/windowing/window.h
+++ Projects/engine/src/core/windowing/window.h
@@ -1,14 +1,19 @@
 #pragma once
+#include "engine_api.h"
 #include "window_items/window_data.h"
 #include "window_items/window_properties.h"
 
 #include "src/types/boolean.h"
+
+struct platform_window_props;
 
 typedef struct window {
     window_data data;
     window_properties properties;
 
     bool is_open;
+
+    struct platform_window_props* platform_properties;
 } window;
 
 window window_create();
@@ -16,7 +21,7 @@ void window_open(window* win);
 void window_update(window* win);
 void window_close(window* win);
 
-extern bool window_is_open(window* win) { return win->is_open; }
+HEADER_DEF bool window_is_open(window* win) { return win->is_open; }
 
-extern window_data* window_get_data(window* win) { return &win->data; }
-extern window_properties* window_get_properties(window* win) { return &win->properties; }
+HEADER_DEF window_data* window_get_data(window* win) { return &win->data; }
+HEADER_DEF window_properties* window_get_properties(window* win) { return &win->properties; }
