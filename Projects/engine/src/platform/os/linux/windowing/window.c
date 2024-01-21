6a59b5f4719b1f489be6bea85fe6b2f7aa9de0bc
Gregory Wells <gregowells07@gmail.com>
2024-01-21 10:40:48 -0500

    write C file

---
+++ Projects/engine/src/platform/os/linux/windowing/window.c
@@ -0,0 +1,23 @@
+#include "src/platform/platform.h"
+
+#if OS == OS_LINUX
+#include "src/core/windowing/window.h"
+
+struct platform_window_props {
+    
+};
+
+window window_create() {
+
+}
+void window_open(window* win) {
+
+}
+void window_update(window* win) {
+
+}
+void window_close(window* win) {
+
+}
+
+#endif
