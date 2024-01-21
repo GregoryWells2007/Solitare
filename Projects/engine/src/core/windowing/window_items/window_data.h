3cce1f0b8631b4502f85e4f12aa0a10ee6e09d24
Gregory Wells <gregowells07@gmail.com>
2024-01-21 10:28:52 -0500

    add window data

--- Projects/engine/src/core/windowing/window_items/window_data.h
+++ Projects/engine/src/core/windowing/window_items/window_data.h
@@ -2,8 +2,9 @@
 #include "src/types/vector2.h"
 
 typedef struct window_data {
-
+    char* title;
+    vector2 size;
 } window_data;
 
-extern void window_data_set_name(window_data* data, char* value) {}
-extern void window_data_set_size(window_data* data, vector2 size) {}
+extern void window_data_set_name(window_data* data, char* value) { data->tile = value; }
+extern void window_data_set_size(window_data* data, vector2 value) { data->size = value; }
