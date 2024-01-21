0a730ba6ae681bccc81e37dbb54ad6be7b116c1b
Gregory Wells <gregowells07@gmail.com>
2024-01-21 10:54:06 -0500

    os specific rendering APIs

--- Projects/engine/src/platform/platform.h
+++ Projects/engine/src/platform/platform.h
@@ -5,13 +5,22 @@
 #define OS_WINDOWS 0x1ef
 #define OS_MACOS 0x197
 
+#define OS OS_LINUX
 // rendering API
 
+#if OS == OS_LINUX
 #define RAPI_OPENGL 0x140
 #define RAPI_VULKAN 0x14c
-#define RAPI_METAL 0x11e
-#define RAPI_DIRECTX 0x15e
+#endif
 
-// build platforms
-#define OS OS_LINUX
+#if OS == OS_WINDOWS
+#define RAPI_OPENGL 0x140
+#define RAPI_VULKAN 0x14c
+#define RAPI_DIRECTX 0x15e
+#endif
+
+#if OS == OS_MACOS
+#define RAPI_METAL 0x11e
+#endif
+
 #define RAPI RAPI_OPENGL
