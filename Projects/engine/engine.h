bbe1763d3123eafae73b3653db97f71f145e070a
Gregory Wells <gregowells07@gmail.com>
2024-01-25 20:44:31 -0500

    add includes

--- Projects/engine/engine.h
+++ Projects/engine/engine.h
@@ -3,4 +3,8 @@
 #include "src/platform/platform.h"
 #include "src/types/vector2.h"
 
+// window includes
 #include "src/core/windowing/window.h"
+
+// rendering includes
+#include "src/core/rendering/output_buffer/output_buffer.h"
