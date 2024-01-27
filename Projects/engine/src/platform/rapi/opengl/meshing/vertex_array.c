89dcaa2f83e96791674f9f3f8955d8f3c80ffd13
Gregory Wells <gregowells07@gmail.com>
2024-01-27 10:51:47 -0500

    create and delete vertex arrays

--- Projects/engine/src/platform/rapi/opengl/meshing/vertex_array.c
+++ Projects/engine/src/platform/rapi/opengl/meshing/vertex_array.c
@@ -1,19 +1,22 @@
 #include "engine_api.h"
 
 #if RAPI == RAPI_OPENGL
-
 #include "src/core/rendering/meshing/vertex_array.h"
+#include "glad/glad.h"
 
 struct platform_vertex_array {
-
+    unsigned int rendererID;
 };
 
 void platform_vertex_array_build(veretx_array* array) {
-    printf("building platform vertex array\n");
+    array->platform_vertex_array = malloc(sizeof(struct platform_vertex_array));
+
+    glGenVertexArrays(1, &array->platform_vertex_array->rendererID);
+    glBindVertexArray(array->platform_vertex_array->rendererID);
 }
 
 void platform_vertex_array_delete(veretx_array* array) {
-    printf("deleting platform vertex array\n");
+    glDeleteVertexArrays(1, &array->platform_vertex_array->rendererID);
 }
 
 #endif
