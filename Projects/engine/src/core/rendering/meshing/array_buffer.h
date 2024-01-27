473075d8b81e545e88e2a5a8743cffad459c7063
Gregory Wells <gregowells07@gmail.com>
2024-01-26 21:13:01 -0500

    add set_data function

--- Projects/engine/src/core/rendering/meshing/array_buffer.h
+++ Projects/engine/src/core/rendering/meshing/array_buffer.h
@@ -1,10 +1,13 @@
 #pragma once
+#include "engine_api.h"
 
 typedef struct array_buffer {
-
+    void* data;
 } array_buffer;
 
-array_buffer array_buffer_create() {
+HEADER_DEF array_buffer array_buffer_create() {
     array_buffer new_array_buffer = (array_buffer){ };
     return new_array_buffer;
 }
+
+HEADER_DEF void array_buffer_set_data(array_buffer* buffer, void* data) { buffer->data = data; }
