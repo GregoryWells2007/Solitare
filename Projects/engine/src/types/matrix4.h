13595a6b1b1d4d1079c22d6743b809dac10e88f9
Gregory Wells <gregowells07@gmail.com>
2024-02-09 21:00:40 -0500

    create identity matrix

--- Projects/engine/src/types/matrix4.h
+++ Projects/engine/src/types/matrix4.h
@@ -1,5 +1,32 @@
 #pragma once
+#include "engine_api.h"
 
 typedef struct matrix4 {
     float values[4][4];
 } matrix4;
+
+HEADER_DEF matrix4 matrix4_identity() {
+    matrix4 new_matrix4 = (matrix4) { };
+
+    new_matrix4.values[0][0] = 1.0f;
+    new_matrix4.values[0][1] = 0.0f;
+    new_matrix4.values[0][2] = 0.0f;
+    new_matrix4.values[0][3] = 0.0f;
+
+    new_matrix4.values[1][0] = 0.0f;
+    new_matrix4.values[1][1] = 1.0f;
+    new_matrix4.values[1][2] = 0.0f;
+    new_matrix4.values[1][3] = 0.0f;
+
+    new_matrix4.values[2][0] = 0.0f;
+    new_matrix4.values[2][1] = 0.0f;
+    new_matrix4.values[2][2] = 1.0f;
+    new_matrix4.values[2][3] = 0.0f;
+
+    new_matrix4.values[3][0] = 0.0f;
+    new_matrix4.values[3][1] = 0.0f;
+    new_matrix4.values[3][2] = 0.0f;
+    new_matrix4.values[3][3] = 1.0f;
+
+    return new_matrix4;
+}
