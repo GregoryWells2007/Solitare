59b01868c5be6d77483e3fcb6a3c361c396b3583
Gregory Wells <gregowells07@gmail.com>
2024-02-10 17:07:45 -0500

    vecctor2 equal functions

--- Projects/engine/src/types/vector2.h
+++ Projects/engine/src/types/vector2.h
@@ -1,11 +1,5 @@
 #pragma once
-
-typedef struct vector2i {
-    union {
-        struct { int x, y; };    
-        struct { int a, b; };     
-    };
-} vector2i;
+#include "engine_api.h"
 
 typedef struct vector2f {
     union {
@@ -23,7 +17,16 @@ typedef vector2f Vector2;
 typedef vector2f Float2;
 typedef vector2f Vec2;
 
+HEADER_DEF bool vector2f_equals(vector2f v1, vector2f v2) { return (v1.x == v2.x) && (v1.y == v2.y); }
+
 /////// -------------- //
+
+typedef struct vector2i {
+    union {
+        struct { int x, y; };    
+        struct { int a, b; };     
+    };
+} vector2i;
 
 typedef vector2i int2;
 typedef vector2i ivec2;
@@ -33,3 +36,5 @@ typedef vector2i vector2int;
 typedef vector2i Vector2int;
 typedef vector2i Int2;
 typedef vector2i IVec2;
+
+HEADER_DEF bool vector2i_equals(vector2i v1, vector2i v2) { return (v1.x == v2.x) && (v1.y == v2.y); }
