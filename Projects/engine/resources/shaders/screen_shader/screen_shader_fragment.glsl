4dc55d01342a00177d19f9f11c4ee89b6185acb8
Gregory Wells <gregowells07@gmail.com>
2024-02-09 20:06:33 -0500

    make simple vignette effect

--- Projects/engine/resources/shaders/screen_shader/screen_shader_fragment.glsl
+++ Projects/engine/resources/shaders/screen_shader/screen_shader_fragment.glsl
@@ -5,6 +5,13 @@ out vec4 color;
 in vec2 vUV;
 uniform sampler2D texure;
 
+uniform float vignette_size;
+uniform float vignette_power;
+
 void main(void) {
-   color = texture(texure, vUV);
+	vec2 uv = (1.0 - vUV) * vUV;
+   float vignette_coverage = uv.x * uv.y * 15.0;
+   float vignette_filter = pow(vignette_coverage, 0.25);
+
+   color = texture(texure, vUV) * vec4(vec3(vignette_filter), 1.0); 
 }
