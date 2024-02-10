81fae998f700e662bf0bc830b5e5ca27bccf743b
Gregory Wells <gregowells07@gmail.com>
2024-02-09 20:27:32 -0500

    add uniforms

--- Projects/engine/resources/shaders/screen_shader/screen_shader_fragment.glsl
+++ Projects/engine/resources/shaders/screen_shader/screen_shader_fragment.glsl
@@ -5,13 +5,15 @@ out vec4 color;
 in vec2 vUV;
 uniform sampler2D texure;
 
-uniform float vignette_size;
 uniform float vignette_power;
+uniform bool use_vignette;
 
 void main(void) {
 	vec2 uv = (1.0 - vUV) * vUV;
-   float vignette_coverage = uv.x * uv.y * 15.0;
-   float vignette_filter = pow(vignette_coverage, 0.25);
+   float vignette_coverage = uv.x * uv.y;
+   float vignette_filter = pow(vignette_coverage, vignette_power);
 
-   color = texture(texure, vUV) * vec4(vec3(vignette_filter), 1.0); 
+   color = texture(texure, vUV); 
+   if (use_vignette)
+      color *= vec4(vec3(vignette_filter), 1.0);
 }
