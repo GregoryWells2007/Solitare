f7e9eefcc11b63600e774748f32321d553dd72db
Gregory Wells <gregowells07@gmail.com>
2024-02-11 16:10:33 -0500

    allow dynamic amount of areas

--- Projects/game/src/board/board.h
+++ Projects/game/src/board/board.h
@@ -6,9 +6,16 @@ struct board_rendering_data {
     texture_2d board_areas_image;
 };
 
+typedef struct board_area {
+    ivec2 position;
+    int card_index;
+} board_area;
+
 typedef struct board {
     char* background_image;
     char* areas_image;
+
+    board_area* areas;
 
     struct board_rendering_data board_rendering_data;
 } board;
