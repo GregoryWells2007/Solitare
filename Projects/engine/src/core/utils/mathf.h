ab20c2945c2c20aa9f2d611bad97c70a71571eef
Gregory Wells <gregowells07@gmail.com>
2024-02-10 19:15:36 -0500

    oops forgot to return

--- Projects/engine/src/core/utils/mathf.h
+++ Projects/engine/src/core/utils/mathf.h
@@ -2,5 +2,5 @@
 #include "engine_api.h"
 #include <math.h>
 
-HEADER_DEF float radians(float degrees) { degrees * 0.01745329251994329576923690768489; }
-HEADER_DEF float degrees(float radians) { radians * 57.2957795130823208767981548141050; }
+HEADER_DEF float radians(float degrees) { return degrees * 0.01745329251994329576923690768489; }
+HEADER_DEF float degrees(float radians) { return radians * 57.2957795130823208767981548141050; }
