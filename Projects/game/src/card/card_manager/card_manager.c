49811bdeb23a4c2a01f387128c455936fd963eca
Gregory Wells <gregowells07@gmail.com>
2024-02-18 15:42:45 -0500

    put cards in stack

--- Projects/game/src/card/card_manager/card_manager.c
+++ Projects/game/src/card/card_manager/card_manager.c
@@ -35,10 +35,15 @@ void card_manager_init(card_manager* manager) {
             index++;
         }
     }
-    // struct card_data* new_data = malloc(sizeof(struct card_data));
-    // new_data->flipped = true;
-    // new_data->number = 10;
-    // linked_list_add(&manager->cards_in_stack, new_data);
+
+    for (int i = 0; i < 24; i++) {
+        struct card_data* new_data = malloc(sizeof(struct card_data));
+        new_data->flipped = true;
+        new_data->number = numbers[index];
+
+        linked_list_add(&manager->cards_in_stack, new_data);     
+        index++;       
+    }
 }
 
 void card_manager_draw_cards(card_manager* manager) {
