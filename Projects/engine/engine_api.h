2ef37ee0244e641dc56842a7466a7c3e8391e023
Gregory Wells <gregowells07@gmail.com>
2024-02-18 16:24:45 -0500

    min and max functions

--- Projects/engine/engine_api.h
+++ Projects/engine/engine_api.h
@@ -10,6 +10,5 @@
 #include <sys/time.h>  
 
 #define HEADER_DEF __attribute__((weak))
-HEADER_DEF int min (int v1, int v2) {
-    return (v1 > v2) ? v1 : v2;
-}
+HEADER_DEF int min (int v1, int v2) { return (v1 < v2) ? v1 : v2; }
+HEADER_DEF int max (int v1, int v2) { return (v1 > v2) ? v1 : v2; }
