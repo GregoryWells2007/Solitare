c088b6e6f71e33a4b82d27f835c4d4ba8195253a
Gregory Wells <gregowells07@gmail.com>
2024-02-22 18:17:38 -0500

    add mouse booleans

--- Projects/engine/src/core/input_manager/input_manager.h
+++ Projects/engine/src/core/input_manager/input_manager.h
@@ -4,8 +4,14 @@
 
 typedef struct input_manager {
     ivec2 mouse_position;
+
+    bool mouse_down, mouse_up;
+    bool mouse_clicked;
 } input_manager;
 
-HEADER_DEF input_manager input_manager_create() { return (input_manager) { 0, 0 }; }
+HEADER_DEF input_manager input_manager_create() { return (input_manager) { }; }
 void input_manager_set_mouse_pos(input_manager* manager, ivec2 mosue_pos);
 ivec2 input_manager_get_mouse_pos(input_manager* manager);
+
+void input_manager_click_mouse(input_manager* manager);
+void input_manager_release_mouse(input_manager* manager);
