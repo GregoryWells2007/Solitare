2f02e23e1142feee4098160ff414832cfb33cba7
Gregory Wells <gregowells07@gmail.com>
2024-02-22 21:37:25 -0500

    add card in mouse bounds

--- Projects/game/src/card/card_manager/card_manager.h
+++ Projects/game/src/card/card_manager/card_manager.h
@@ -27,3 +27,5 @@ void card_manager_init(card_manager* manager);
 void card_manager_draw_cards(card_manager* manager);
 void card_manager_flip_stack(card_manager* manager);
 void card_manager_remove_from_stack(card_manager* manager);
+
+bool mouse_in_bounds(ivec2 card_position, ivec2 mouse_positon);
